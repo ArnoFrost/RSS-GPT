ed4e026a1086f9e99611bc6a69105848b8f18827
xuxin14 <xuxin14@staff.sina.com>
2023-11-10 12:27:21 +0800

    Pref(UI):样式优化测试

--- main.py
+++ main.py
@@ -160,13 +160,15 @@ def gpt_summary(query, model, language):
         messages = [
             {"role": "user", "content": query},
             {"role": "assistant",
-             "content": f"请用中文总结这篇文章，先提取出{keyword_length}个关键词，在同一行内输出，然后换行，用中文在{summary_length}字内写一个包含所有要点的总结，按顺序分要点输出，并按照以下格式输出'<br><br>总结:'，<br>是HTML的换行符，输出时必须保留2个，并且必须在'总结:'二字之前"}
+             "content": f"请用中文总结这篇文章，首先提取出{keyword_length}个关键词，并将它们列成一个列表。然后，在{summary_length}字内写一个包含所有要点的总结。请将关键词和总结分开，并使用以下格式：'关键词：[关键词1], [关键词2], [关键词3], ...<br><br>总结：<br>[总结内容]'。"
+             }
         ]
     else:
         messages = [
             {"role": "user", "content": query},
             {"role": "assistant",
-             "content": f"Please summarize this article in {language} language, first extract {keyword_length} keywords, output in the same line, then line break, write a summary containing all the points in {summary_length} words in {language}, output in order by points, and output in the following format '<br><br>Summary:' , <br> is the line break of HTML, 2 must be retained when output, and must be before the word 'Summary:'"}
+             "content": f"Please summarize this article in {language}, starting with extracting {keyword_length} keywords and listing them. Then, write a summary that contains all the main points in {summary_length} words. Format your output by first presenting the keywords, followed by a line break, and then the summary. Use the following format: 'Keywords: [keyword1], [keyword2], [keyword3], ...<br><br>Summary:<br>[Summary content]'."
+             }
         ]
     client = OpenAI(
         api_key=OPENAI_API_KEY,
